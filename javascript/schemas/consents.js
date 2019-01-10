65d0a9182463c0bcdb5291376d8cce2742f6ba08
sergehuber <shuber@jahia.com>
2019-01-10 10:17:46 +0100

    - Clarifications in specification document - Setup gradle wrapper properly - Cleanup consents types

--- javascript/schemas/consents.js
+++ javascript/schemas/consents.js
@@ -5,15 +5,12 @@ enum CDP_ConsentStatus {
     REVOKED
 }
 
-"""
-Token: Similar to OAuth 2 authorization tokens to access the consent without the profile, also useful to delete the consent
-Type: Should be a Url or other meaningful identifier "//mycompany.com/consents/newsletters/weekly", "//crmcompany.com/consents/push-to-crm", "//oasis_open.org/cxs/consents/send-to-third-parties"
-"""
-
 type CDP_Consent {
+  "Similar to OAuth 2 authorization tokens to access the consent without the profile, also useful to delete the consent"
   token : ID!
   source : CDP_Source
   client : CDP_Client
+  "Should be a Url or other meaningful identifier "//mycompany.com/consents/newsletters/weekly", "//crmcompany.com/consents/push-to-crm", "//oasis_open.org/cxs/consents/send-to-third-parties""
   type : String!
   status : CDP_ConsentStatus!
   statusDate : String
